038d1ed8451f6000b355ecbc33afe7ff713a762d
shanaglazer <shanaglazer1@gmail.com>
2023-11-27 13:04:15 +0200

    10/37

---
+++ MemoryGame/MemoryWeb/Pages/game.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MemoryWeb.Pages
+{
+    public class gameModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- MemoryMaui/Memory.xaml.cs
+++ MemoryMaui/Memory.xaml.cs
@@ -5,35 +5,40 @@ namespace MemoryMaui;
 
 public partial class Memory : ContentPage
 {
-    Game game = new();
+    Game activegame;
+    List<Game> lstgame = new() { new Game(), new Game(), new Game() };
 
     List<Button> lstbuttons;
 
     public Memory()
 	{
 		InitializeComponent();
-        this.BindingContext = game;
+        Game1Rb.BindingContext = lstgame[0];
+        Game2Rb.BindingContext = lstgame[1];
+        Game3Rb.BindingContext = lstgame[2];
+        activegame = lstgame[0];
+        this.BindingContext = activegame;
         lstbuttons = new() { btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10, btn11, btn12, btn13, btn14, btn15, btn16, btn17, btn18, btn19, btn20, btn21, btn22, btn23, btn24, btn25, btn26, btn27, btn28, btn29, btn30 };
     }
 
     private void StartGame()
     {
-        game.StartGame();
+        activegame.StartGame();
         StartGameBtn.IsEnabled = false;
     }
 
     private void DoTurn(Button btn)
     {
         int index = lstbuttons.IndexOf(btn);
-        game.TakeTurn(index);
-        SwitchTurnBtn.IsEnabled = game.EnableBtnSwitch;
+        activegame.TakeTurn(index);
+        SwitchTurnBtn.IsEnabled = activegame.EnableBtnSwitch;
     }
 
     private void SwitchTurn()
     {
         SwitchTurnBtn.IsEnabled = false;
-        game.SwitchTurn();
-        if (game.Winner != Game.TurnEnum.None)
+        activegame.SwitchTurn();
+        if (activegame.Winner != Game.TurnEnum.None)
         {
             StartGameBtn.IsEnabled = true;
         }
@@ -56,4 +61,14 @@ public partial class Memory : ContentPage
 	{
         SwitchTurn();
 	}
+
+    private void Game_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        RadioButton rb = (RadioButton)sender;
+        if(rb.IsChecked && rb.BindingContext != null)
+        {
+            activegame = (Game)rb.BindingContext;
+            this.BindingContext = activegame;
+        }
+    }
 }
